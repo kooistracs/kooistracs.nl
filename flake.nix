0b9b72be6b692ef81ce575a9b6b0b3ca7b8733da
Connor Kooistra <70811244+cdkooistra@users.noreply.github.com>
2025-07-05 11:14:07 +0200

    chore: add nix flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Zola SSG dev environment";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/25.05";
+  
+  outputs = inputs:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+      forEachSupportedSystem = f: inputs.nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import inputs.nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            zola
+          ];
+        };
+      });
+    };
+}
